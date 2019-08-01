44c4cd4c51b70a11335e6751ec7a0899f274aa6b
diogro <diogro@gmail.com>
2019-08-01 17:26:49 -0300

    minor bug

--- multiPeakSimulation.R
+++ multiPeakSimulation.R
@@ -41,8 +41,8 @@ n_peaks = 20
 scale = 6
 runSimulation = function(G_type = c("Diagonal", "Integrated"), G = NULL,
                          n_peaks = 1, p, rho = 0.7, scale = 6){
+    G_type = match.arg(G_type)
     if(is.null(G)){
-        G_type = match.arg(G_type)
         if(G_type == "Diagonal"){
             G = diag(p)
             diag(G) = rnorm(p, 1, 0.1)
@@ -59,7 +59,7 @@ runSimulation = function(G_type = c("Diagonal", "Integrated"), G = NULL,
         Surface_type = "Single"
     } else
         Surface_type = "Muliple"
-    theta = matrix(runif(p*n_peaks, -8, 8), n_peaks, p, byrow = T)
+    theta = matrix(runif(p*n_peaks, -space_size, space_size), n_peaks, p, byrow = T)
     W_bar = W_bar_factory(theta)
     W_bar_grad = W_bar_gradient_factory(theta)
     trajectory = calculateTrajectory(rep(0, p), G, W_bar, W_bar_grad, scale = scale)
@@ -73,12 +73,12 @@ runSimulation = function(G_type = c("Diagonal", "Integrated"), G = NULL,
     trajectory$Surface_type = Surface_type
     return(trajectory)
 }
-runSimulation("Integrated", NULL, 10, 2, scale = 10)
+runSimulation("Integrated", G_corr, n_peaks = 2, n_traits, scale = 10)
 
-results_G_diag_W_single = rlply(1000, runSimulation("Diagonal"  , G_diag, 1 , n_traits, scale = 2), .parallel = TRUE)
-results_G_corr_W_single = rlply(1000, runSimulation("Integrated", G_corr, 1 , n_traits, scale = 4), .parallel = TRUE)
-results_G_diag_W_multi  = rlply(1000, runSimulation("Diag"      , G_diag, 200, n_traits, scale = 2), .parallel = TRUE)
-results_G_corr_W_multi  = rlply(1000, runSimulation("Integrated", G_corr, 200, n_traits, scale = 6), .parallel = TRUE)
+results_G_diag_W_single = llply(1:1000, function(x) runSimulation("Diag", G_diag,   1, n_traits, scale = 2), .parallel = TRUE)
+results_G_corr_W_single = llply(1:1000, function(x) runSimulation("Inte", G_corr,   1, n_traits, scale = 4), .progress = "text")
+results_G_diag_W_multi  = llply(1:1000, function(x) runSimulation("Diag", G_diag, 200, n_traits, scale = 2), .parallel = TRUE)
+results_G_corr_W_multi  = llply(1:1000, function(x) runSimulation("Inte", G_corr, 200, n_traits, scale = 6), .progress = "text")
 
 par(mfrow=c(2, 2))
 plotDzgmax_normdz(results_G_diag_W_single, ylim = c(0, 30), main = "Diagonal G - Single Peak")
