7dfea874f9820e3b24a4d03e62c3e26a750f8bb0
diogro <diogro@gmail.com>
2019-08-29 16:33:04 -0300

    new figures for supporting info

--- simulation_Ilustration.R
+++ simulation_Ilustration.R
@@ -1,10 +1,18 @@
 source("./trajectoryTools.R")
+if(!require(magick)){install.packages("magick"); library(magick)}
+
+
+################
+# Peak distribution
+################
 
 space_size = 10
 npeaks = 100
 p = 2
 
 uniform_peaks = matrix(runif(p*npeaks, -10, 10), npeaks, p)
+
+png("./plots/peaks_square.png", width = 1080, height = 1080)
 plot(uniform_peaks, axes = FALSE, pch = 17, xlab = "", ylab= "", xlim=c(-10.1, 10.1), ylim = c(-10.1, 10.1), asp = 1)
 polygon(x = matrix(c(-10, -10, 
                      -10, 10,
@@ -13,20 +21,58 @@ polygon(x = matrix(c(-10, -10,
                      -10, -10), 5, 2, byrow = T))
 abline(v=0)
 abline(h=0)
+dev.off()
 
 peaks = randomPeaks(npeaks, p, intervals = c(1), prop = c(1), dz_lim = c(0, 10))
+
+png("./plots/peaks_circle.png", width = 1080, height = 1080)
 plot(peaks, axes = FALSE, pch = 17, xlab = "", ylab= "", xlim=c(-10.1, 10.1), ylim = c(-10.1, 10.1), asp = 1)
 draw.circle(0, 0, 10, nv = 1000, border = NULL, col = NA, lty = 1, lwd = 1)
 abline(v=0)
 abline(h=0)
+dev.off()
 
 peaks = randomPeaks(npeaks, p, intervals = c(1), prop = c(1), dz_lim = c(3, 10))
+
+png("./plots/peaks_ring.png", width = 1080, height = 1080)
 plot(peaks, axes = FALSE, pch = 17, xlab = "", ylab= "", xlim=c(-10.1, 10.1), ylim = c(-10.1, 10.1), asp = 1)
 draw.circle(0, 0, 10, nv = 1000, border = NULL, col = NA, lty = 1, lwd = 1)
 draw.circle(0, 0, 3, nv = 1000, border = NULL, col = NA, lty = 1, lwd = 1)
 abline(v=0)
 abline(h=0)
-gplotW_bar(peaks, 10)
+dev.off()
+
+peaks_ring_surface = gplotW_bar(peaks, 10)
+save_plot("./plots/peaks_ring_surface.png", peaks_ring_surface, base_height = 9, base_asp = 1)
+
+
+####################
+# Single Trajectory
+####################
+
+set.seed(1)
+peak = matrix(c(2.5, 5), 1, 2)
+y = runSimulation("Integrated", rho = 0.75, p = 2, scale = 4, theta = peak)
+ex_traj = gplotW_bar_trajectory(y, space_size, xlimits = c(-7, space_size), ylimits = c(-3, space_size), main = "")
+ex_traj = ex_traj + annotate("segment", x = -1, y = -1, yend = - 0.1, xend = - 0.1, arrow=arrow()) + 
+      annotate("text", x = -1.3, y = -1.3, label = "Starting point") + 
+  draw_image("./plots/G_inte.png",
+    x = -6, y = 6, width = 5, height = 5 )
+save_plot("./plots/ex_trajectory_int.png", ex_traj, base_height = 9, base_asp = 1)
+
+set.seed(1)
+x = runSimulation("Diagonal", rho = 0, p = 2, scale = 4, theta = peak)
+ex_traj = gplotW_bar_trajectory(x, space_size, xlimits = c(-7, space_size), ylimits = c(-3, space_size), main = "") 
+ex_traj = ex_traj + annotate("segment", x = -1, y = -1, yend = - 0.1, xend = - 0.1, arrow=arrow()) + 
+  annotate("text", x = -1.3, y = -1.3, label = "Starting point") + 
+  draw_image("./plots/G_diag.png",
+             x = -6, y = 6, width = 5, height = 5 )
+save_plot("./plots/ex_trajectory_diag.png", ex_traj, base_height = 9, base_asp = 1)
+
+##########
+# Trajectory Examples
+#########
+
 
 space_size = 10
 npeaks = 50
